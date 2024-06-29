3b2633623e0015f81a97caffcc730f4e37f13f7b
Hiroki Tei <hirokiki18@gmail.com>
2024-06-29 16:43:01 +0900

    implement frontend

--- chainlit/app/custom_front/front/src/App.tsx
+++ chainlit/app/custom_front/front/src/App.tsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
+import { sessionState, useChatSession } from "@chainlit/react-client";
+import { Playground } from '@/page/playground'
+import { useRecoilValue } from "recoil";
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { connect } = useChatSession();
+  const session = useRecoilValue(sessionState);
+  
+  const userEnv = {}
+  useEffect(() => {
+    if (session?.socket.connected) {
+      return;
+    }
+    fetch("http://localhost:80/custom-auth", {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        connect({
+          userEnv,
+          accessToken: `Bearer: ${data.token}`,
+        });
+      });
+  }, [connect]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Playground/>
     </>
   )
 }
